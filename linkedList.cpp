db515b028d66805477969d4357af44d175d02893
Ismail AliF <exh.alif.47@gmail.com>
2023-12-13 21:57:04 +0600

    kinda fixed ig, empty list er exception handle kori nai

--- linkedList.cpp
+++ linkedList.cpp
@@ -45,9 +45,39 @@ void insertAt(node* &root, int target, int val) {
 }
 
 
-void deleteAt(node* &root, int target) {
-    node* temp = root;
+int listSize(node* &root) {
     int tracker = 1;
+    node* temp = root;
+    if (temp == NULL) return 0;
+    while (temp->next != NULL) {
+        temp = temp->next;
+        tracker++;
+    }
+    return tracker;
+}
+
+
+
+void deleteAt(node* &root, int target) {
+
+    int size = listSize(root);
+    node* temp = root;
+    
+    if (target == 1) {
+        root = root->next;
+        cout << "Deleted Node at " << target << endl;
+        return;
+    }
+
+
+    else if (target == size) {
+        while (temp->next->next!= NULL) temp = temp->next;
+        temp->next = NULL;
+        cout << "Deleted Node at " << target << endl;
+        return;
+    }
+
+    int tracker = 2;
     while (temp->next != NULL) {
         if (tracker == target)
             break;
@@ -99,6 +129,10 @@ node* previousOfLastNode(node* &root) {
 // gotta think for list with 1 element;
 // ***************************************
 void deleteLast(node* &root) {
+    if (root->next == NULL) {
+        root = root->next;
+        return;
+    } 
     node* temp = root;
     while (temp->next->next!= NULL) temp = temp->next;
 
@@ -112,6 +146,10 @@ void deleteFirst(node* &root) {
 }
 
 void printList(node* &root) {
+    if (root == NULL) {
+        cout << "List : " << endl;
+        return;
+    }
     cout << endl;
     cout << "List : ";
     node* temp = root;
@@ -126,12 +164,15 @@ void printList(node* &root) {
 
 
 int main (void) {
+    system("cls");
 
     node* root = new node(1);
 
     for (int i=2; i<=12; i++) listAppend(root, i);
 
     printList(root);
+
+    cout << "List size : " << listSize(root) << endl;
 
     listPrepend(root,-1);
     listPrepend(root,-2);
@@ -145,7 +186,7 @@ int main (void) {
 
     insertAt(root, 2, 10);
     printList(root);
-    deleteAt(root, 4);
+    deleteAt(root, 3);
     printList(root);
 
     listFind(root, 5);
