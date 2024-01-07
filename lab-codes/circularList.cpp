4f819531a6d3c747b7831b03de5ea1c9a2edb433
Ismail AliF <exh.alif.47@gmail.com>
2024-01-08 01:08:02 +0600

    circular

--- lab-codes/circularList.cpp
+++ lab-codes/circularList.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 typedef struct node {
     int data;
     node* next;
-    
+
     node(int val) {
         data = val;
         next = NULL;
@@ -13,174 +13,174 @@ typedef struct node {
 
 node* root = NULL;
 
-void listAppend(int val) {
+void insertFirst(int val) {
     node* newNode = new node(val);
-
-    node* temp = root;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = newNode;
-    cout << val << " inserted at the end" << endl;
-}
-
-void listPrepend(int val) {
-    node* newNode = new node(val);
-    newNode->next = root;
-    root = newNode;
-    cout << val << " inserted at the beginning" << endl;
-}
-
-void insertAt(int target, int val) {
-    node* newNode = new node(val);
-    node* temp = root;
-    int tracker = 1;
-    while (temp->next != NULL) {
-        if (tracker == target)
-            break;
-        temp = temp->next;
-        tracker++;
-    }
-    newNode->next = temp->next;
-    temp->next = newNode;
-    cout << val << " inserted at " << target << endl;
-}
-
-
-int listSize() {
-    int tracker = 1;
-    node* temp = root;
-    if (temp == NULL) return 0;
-    while (temp->next != NULL) {
-        temp = temp->next;
-        tracker++;
-    }
-    return tracker;
-}
-
-
-
-void deleteAt(int target) {
-
-    int size = listSize(root);
-    node* temp = root;
     
-    if (target == 1) {
-        root = root->next;
-        cout << "Deleted Node at " << target << endl;
-        return;
-    }
-
-
-    else if (target == size) {
-        while (temp->next->next!= NULL) temp = temp->next;
-        temp->next = NULL;
-        cout << "Deleted Node at " << target << endl;
-        return;
-    }
-
-    int tracker = 2;
-    while (temp->next != NULL) {
-        if (tracker == target)
-            break;
-        temp = temp->next;
-        tracker++;
-    }
-    if (temp->next->next != NULL) {
-        temp->next = temp->next->next;
-    }
-    cout << "Deleted Node at " << target << endl;
-}
-
-void listFind(int target){ 
-    node* temp = root;
-    int tracker = 1;
-    while (temp != NULL and temp->data != target) {
-        temp = temp->next;
-        tracker++;
-    }
-
-    if (temp == NULL) {
-        cout << target << " not found";
-    } 
-    else {
-        cout << target << " found at " << tracker << endl;
+    if (!root) {
+        root = newNode;
+        newNode->next = root; 
+    } else {
+        newNode->next = root->next;
+        root->next = newNode;
     }
 }
 
-node* lastNode() {
-    node* temp = root;
-    while (temp->next != NULL) temp = temp->next;
-    return temp;
-}
-
-void printNode(node* nd) {
-    if (nd != NULL)
-        cout << nd->data << endl;
-}
-
-node* previousOfLastNode() {
-    node* temp = root;
-    if (temp->next == NULL) return NULL;
-    while (temp->next->next != NULL)
-        temp = temp->next;
-    return temp;
-}
-
-void deleteLast() {
-    if (root->next == NULL) {
-        root = root->next;
-        return;
-    } 
-    node* temp = root;
-    while (temp->next->next!= NULL) temp = temp->next;
-
-    temp->next = NULL;
-    cout << "Last Node Deleted!" << endl;
+void insertLast(int val) {
+    node* newNode = new node(val);
+    
+    if (!root) {
+        root = newNode;
+        newNode->next = root;
+    } else {
+        newNode->next = root->next;
+        root->next = newNode;
+        root = newNode;
+    }
 }
 
 void deleteFirst() {
-    root = root->next;
-    cout << "First Node Deleted!" << endl;
+    if (!root) {
+        cout << "Cannot delete" << endl;
+        return;
+    }
+
+    if (root->next == root) {
+        delete root;
+        root = NULL;
+    } else {
+        node* temp = root->next;
+        root->next = temp->next;
+        delete temp;
+    }
 }
 
+
+void deleteLast() {
+    if (!root) {
+        cout << "Cannot delete" << endl;
+        return;
+    }
+
+    if (root->next == root) {
+        delete root;
+        root = NULL;
+    } else {
+        node* current = root->next;
+        while (current->next != root) {
+            current = current->next;
+        }
+        current->next = root->next;
+        delete root;
+        root = current;
+    }
+}
+
+void insertAt(int position, int value) {
+    if (position < 1) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+
+    node* newNode = new node(value);
+    
+    if (!root) {
+        root = newNode;
+        newNode->next = root;  // Pointing back to itself for circularity
+    } else {
+        node* current = root->next;
+        for (int i = 1; i < position - 1; ++i) {
+            if (current == root)
+                return;
+
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+}
+
+void deleteAt(int position) {
+    if (!root || position < 1) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+
+    if (position == 1) {
+        deleteFirst();
+    } else {
+        node* current = root->next;
+        for (int i = 1; i < position - 1; ++i) {
+            if (current == root) {
+                cout << "Invalid position. Cannot delete.\n";
+                return;
+            }
+            current = current->next;
+        }
+        node* temp = current->next;
+        current->next = temp->next;
+        delete temp;
+    }
+}
+
+
+void printSize() {
+    if (!root) {
+        cout << "Size : 0" << endl;
+        return;
+    }
+
+    int size = 0;
+    node* current = root->next;
+
+    do {
+        size++;
+        current = current->next;
+    } while (current != root->next);
+
+    cout << "Size : " << size << endl;
+}
+
+
 void printList() {
-    if (root == NULL) {
+    if (!root) {
         cout << "List : " << endl;
         return;
     }
-    cout << endl;
+
+    node* current = root->next;
+
     cout << "List : ";
-    node* temp = root;
-    while (temp->next != NULL) {
-        cout << temp->data << " ==> ";
-        temp = temp->next;
-    }
-    cout << temp->data;
-    cout << endl << endl;
+
+    do {
+        cout << current->data << " ";
+        current = current->next;
+    } while (current != root->next);
+
+    cout << endl;
 }
 
+int main() {
+    insertFirst(3);
+    insertFirst(2);
+    insertFirst(1);
 
+    insertLast(4);
+    insertLast(5);
 
-int main (void) {
-    system("cls");
+    printList();
 
-    node* root = new node(1);
-
-    for (int i=2; i<=12; i++) listAppend(i);
-
-    printList(root);
-
-    cout << "List size : " << listSize() << endl;
-
-    listPrepend(-1);
-    listPrepend(-2);
-
+    deleteFirst();
     printList();
 
     deleteLast();
     printList();
-    deleteFirst();
+
+    insertAt(2, 6);
     printList();
+
+    deleteAt(2);
+    printList();
+
+    printSize();
 
 }
